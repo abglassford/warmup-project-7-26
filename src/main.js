c26bb70b6a7a762ffe7817ae5aace44515b69fc4
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-26 13:08:22 -0600

    stuff

--- src/main.js
+++ src/main.js
@@ -5,17 +5,21 @@ $(function () {
     findMovie($('#inputTitle').val())
   })
 })
+
+
 function findMovie (movie) {
-  $.ajax({
-    method: 'GET',
-    url: 'http://www.omdbapi.com/?t=' + movie
-  }).done(function (data) {
-    console.log(data)
-    $('#movieImg').append('<img src="' + data.Poster + '">')
-    $('#movieInfo').append('<p>' + data.Title + '</p>')
-    var genreArr = data.Genre.split(', ')
-    for (var i = 0; i < genreArr.length; i++) {
-      $('#genreSelect').append('<option id= "' + genreArr[i] + '">' + genreArr[i] + '</option>')
-    }
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      method: 'GET',
+      url: 'http://www.omdbapi.com/?t=' + movie
+    }).then(function (data) {
+      console.log(data)
+      $('#movieImg').append('<img src="' + data.Poster + '">').css('float', 'left')
+      $('#movieImg').append('<p>' + data.Title + '</p>')
+      var genreArr = data.Genre.split(', ')
+      for (var i = 0; i < genreArr.length; i++) {
+        $('#genreSelect').append('<option id= "' + genreArr[i] + '">' + genreArr[i] + '</option>')
+      }
+    })
   })
 }
