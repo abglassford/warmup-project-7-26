90d84630f91d03271c1779a4d33136347efeb695
Allen Fordham <allen.s.fordham@gmail.com>
2016-07-26 12:00:02 -0600

    changed a couple formatting things, mostly adding semicolons

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 // document ready
 $(function(){
   $('form').on('submit', function (e){
-    e.preventDefault()
+    e.preventDefault();
     findMovie($('#inputTitle').val());
-  })
-})
+  });
+});
 function findMovie (movie){
   $.ajax({
     method: 'GET',
     url: 'http://www.omdbapi.com/?t=' + movie
   }).done(function(data){
     console.log(data);
-    $('#movieImg').append('<img src="' + data.Poster + '">')
-    $('#movieInfo').append('<p>' + data.Title + '</p>')
-    var genreArr = data.Genre.split(', ')
+    $('#movieImg').append('<img src="' + data.Poster + '">');
+    $('#movieInfo').append('<p>' + data.Title + '</p>');
+    var genreArr = data.Genre.split(', ');
     console.log(genreArr);
     for(var i = 0; i < genreArr.length; i++){
-      $('#genreSelect').append('<option id= "op' + [i] + '">' + genreArr[i] + '</option>')
+      $('#genreSelect').append('<option id= "op' + [i] + '">' + genreArr[i] + '</option>');
       console.log(genreArr[i]);
     }
 
-  })
+  });
 }
