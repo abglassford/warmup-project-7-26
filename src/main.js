a7bd5d11ddb868171ed32b601b97eef457165f30
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-26 10:30:32 -0600

    help

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 $(function(){
-    function findMovie (movie){
-      $.ajax({
-        method: 'GET',
-        url: 'http://www.omdbapi.com/?t=' + movie
-      }).done(function(data){
-        console.log(data);
-      })
-    }
+  function findMovie (movie){
+    $.ajax({
+      method: 'GET',
+      url: 'http://www.omdbapi.com/?t=' + movie
+    }).done(function(data){
+      console.log(data);
+    })
+  }
 })
