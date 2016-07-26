aeff59a687e278ed53badea061157d28d0872a9f
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-26 11:58:05 -0600

    change option id to genre type

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
 // document ready
-$(function(){
-  $('form').on('submit', function (e){
+$(function () {
+  $('form').on('submit', function (e) {
     e.preventDefault()
-    findMovie($('#inputTitle').val());
+    findMovie($('#inputTitle').val())
   })
 })
-function findMovie (movie){
+function findMovie (movie) {
   $.ajax({
     method: 'GET',
     url: 'http://www.omdbapi.com/?t=' + movie
-  }).done(function(data){
-    console.log(data);
+  }).done(function (data) {
+    console.log(data)
     $('#movieImg').append('<img src="' + data.Poster + '">')
     $('#movieInfo').append('<p>' + data.Title + '</p>')
     var genreArr = data.Genre.split(', ')
-    console.log(genreArr);
-    for(var i = 0; i < genreArr.length; i++){
-      $('#genreSelect').append('<option id= "op' + [i] + '">' + genreArr[i] + '</option>')
-      console.log(genreArr[i]);
+    for (var i = 0; i < genreArr.length; i++) {
+      $('#genreSelect').append('<option id= "' + genreArr[i] + '">' + genreArr[i] + '</option>')
     }
-
   })
 }
